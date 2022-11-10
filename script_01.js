fa6716290e731b08f536211f46e00632230067f6
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-10 11:08:41 +0100

    Ternäre Schreibweise eines if elseif else Statements

--- script_01.js
+++ script_01.js
@@ -43,12 +43,12 @@
 /***************************************************************/
 /*********           if elseif else Abfrage           **********/
 /***************************************************************/
-    if ( isJohnOlder ) {                            // logische Abfragen sind True oder Fasle, somit geht das auch ohne Wert oder mit Wert von if ( isJohnOlder == true ) {
-        console.log( "John ist älter" );            // Ja Zweig, wenn die if Condition erreicht ist, dann kommt das zum Tragen
-    } else if ( isJohnEqual ) {                     // logische alternative Abfragen sind True oder Fasle, es können so viele wie benötigt gestellt werden
-        console.log( "John ist gleich alt" );       // Ja Zweig, wenn die else if Condition erreicht ist, dann kommt das zum Tragen
-    } else {                                        // logische Abfrage DANN
-        console.log( "John ist jünger" );           // Sonst Zweig, wenn erste Abfrage nicht erreicht, dann kommt das else zum Tragen
+    if ( isJohnOlder ) {                                // logische Abfragen sind True oder Fasle, somit geht das auch ohne Wert oder mit Wert von if ( isJohnOlder == true ) {
+        console.log( "John ist älter" );                // Ja Zweig, wenn die if Condition erreicht ist, dann kommt das zum Tragen
+    } else if ( isJohnEqual ) {                         // logische alternative Abfragen sind True oder Fasle, es können so viele wie benötigt gestellt werden
+        console.log( "John ist gleich alt" );           // Ja Zweig, wenn die else if Condition erreicht ist, dann kommt das zum Tragen
+    } else {                                            // logische Abfrage DANN
+        console.log( "John ist jünger" );               // Sonst Zweig, wenn erste Abfrage nicht erreicht, dann kommt das else zum Tragen
 }   
 /* Ternäre Schreibweise eines if elseif else Statements */
     console.log( (isJohnOlder) ? "John ist älter" : (isJohnEqual) ? "John ist gleich alt" : "John ist jünger" );
