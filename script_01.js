ecd9aa4f1a887b0cff950fd521bfe8c1fd47c34d
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-10 10:25:26 +0100

    elseif und pap updated

--- script_01.js
+++ script_01.js
@@ -3,15 +3,15 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assigment
-const ageJohn = 35;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+let isJohnOlder, isJohnEqual;               // is oder has Variablen werden immer als geschlossene Frage gestellt: ist John älter? also nur ja oder nein / true / false
 
 // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+isJohnOlder = ( ageJohn > ageMark );          // logische Operatroen, Vergleichsoperatoren wie ist größer
+isJohnEqual = ( ageJohn == ageMark );         // logische Operatroen, Vergleichsoperatoren wie ist exact gleich
 
 // Ausgabe
 // console.log("ageJohn: " + ageJohn);
@@ -20,3 +20,23 @@ isJohnEqual = (ageJohn == ageMark);
 // console.log("isJohnEqual: " + isJohnEqual);
 // console.log("------------------");
 
+/**********   if    **********/
+if ( isJohnOlder ) {                            // logische Abfragen sind True oder Fasle, somit geht das auch ohne Wert oder mit Wert von if ( isJohnOlder == true ) {
+    console.log( "John ist älter" );            
+}
+
+/**********   if else    **********/
+if ( isJohnOlder ) {                            // logische Abfragen sind True oder Fasle, somit geht das auch ohne Wert oder mit Wert von if ( isJohnOlder == true ) {
+    console.log( "John ist älter" );            // Ja Zweig, wenn das if Condition erreicht ist, dann kommt das zum Tragen
+} else {                                        // logische Abfrage DANN
+    console.log( "John ist jünger" );           // Sonst Zweig, wenn erste Abfrage nicht erreicht, dann kommt das else zum Tragen
+}
+
+/**********   if elseif else    **********/
+if ( isJohnOlder ) {                            // logische Abfragen sind True oder Fasle, somit geht das auch ohne Wert oder mit Wert von if ( isJohnOlder == true ) {
+    console.log( "John ist älter" );            // Ja Zweig, wenn das if Condition erreicht ist, dann kommt das zum Tragen
+} else if ( isJohnEqual ) {                     // logische zusätzliche Abfragen sind True oder Fasle, es können so viele wie benötigt gestellt werden
+    console.log( "John ist gleich alt" );       // Ja Zweig, wenn das if Condition erreicht ist, dann kommt das zum Tragen
+} else {                                        // logische Abfrage DANN
+    console.log( "John ist jünger" );           // Sonst Zweig, wenn erste Abfrage nicht erreicht, dann kommt das else zum Tragen
+}
